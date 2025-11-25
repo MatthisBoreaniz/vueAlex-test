99005c4ab0f4288889f197c6b9c9ee23aa5e1eac
Matthis Boreaniz <matthisonepiece@gmail.com>
2025-11-25 08:52:54 +0100

    feat/perso: add btn template

---
+++ src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+    href?: string;
+}>();
+</script>
+
+
+<template>
+    <a v-if="href" class="button" href=""><slot></slot></a>
+    <button v-else class="button"><slot></slot></button>
+</template>
+
+<style lang="scss" >
+.button {
+    padding: 10px 20px;
+    background-color: #42b983;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    text-decoration: none;
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import MyButton from '@/components/MyButton.vue';
+</script>
+
+
 <template>
   <main>
     <h1>HP</h1>
+    <MyButton>Button</MyButton>
+    <MyButton>Link</MyButton>
   </main>
 </template>
