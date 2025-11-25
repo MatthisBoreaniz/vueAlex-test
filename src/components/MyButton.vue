ddf0c70a44474e66d6926dbef7455cd318ca1997
Matthis Boreaniz <matthisonepiece@gmail.com>
2025-11-25 09:48:11 +0100

    design system

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,26 +1,49 @@
 <script setup lang="ts">
 defineProps<{
   href?: string
-  variant?: 'default' | 'outline'
-  size?: 'default' | 'small'
+  variant?: 'default' | 'outline' | 'ghost'
+  size?: 'giant' | 'large' | 'medium' | 'small' | 'tiny'
+  icon?: boolean
+  disabled?: boolean
 }>()
 </script>
 
 <template>
   <a
     v-if="href"
+    :href="href"
     class="btn"
-    :class="{ '-outline': variant === 'outline', '-small': size === 'small' }"
-    href=""
-    ><slot></slot
-  ></a>
+    :class="{
+      '-outline': variant === 'outline',
+      '-ghost': variant === 'ghost',
+      '-giant': size === 'giant',
+      '-large': size === 'large',
+      '-medium': size === 'medium',
+      '-small': size === 'small',
+      '-tiny': size === 'tiny',
+      '-disabled': disabled,
+      '-icon': icon
+    }"
+  >
+    <slot></slot>
+  </a>
+
   <button
     v-else
+    :disabled="disabled"
     class="btn"
-    :class="{ '-outline': variant === 'outline', '-small': size === 'small' }"
+    :class="{
+      '-outline': variant === 'outline',
+      '-ghost': variant === 'ghost',
+      '-giant': size === 'giant',
+      '-large': size === 'large',
+      '-medium': size === 'medium',
+      '-small': size === 'small',
+      '-tiny': size === 'tiny',
+      '-disabled': disabled,
+      '-icon': icon
+    }"
   >
     <slot></slot>
   </button>
 </template>
-
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,52 @@ import MyButton from '@/components/MyButton.vue'
 <template>
   <main>
     <h1>HP</h1>
-    <MyButton>Button</MyButton>
-    <MyButton class="outline">Link</MyButton>
-    <MyButton href="/about" variant="outline">Click me Outline !</MyButton>
-    <MyButton size="small">Click me !</MyButton>
-    <MyButton href="/about" variant="outline" size="small">Click me Outline !</MyButton>
+
+    <div>
+      <MyButton>Default</MyButton>
+      <MyButton disabled>Disabled Default</MyButton>
+      <MyButton variant="outline">Outline</MyButton>
+      <MyButton variant="outline" disabled>Outline Disabled</MyButton>
+      <MyButton variant="ghost">Ghost</MyButton>
+      <MyButton variant="ghost" disabled>Ghost Disabled</MyButton>
+    </div>
+
+    <div>
+      <MyButton size="giant">Giant</MyButton>
+      <MyButton size="large">Large</MyButton>
+      <MyButton size="medium">Medium</MyButton>
+      <MyButton size="small">Small</MyButton>
+      <MyButton size="tiny">Tiny</MyButton>
+    </div>
+
+    <div>
+      <MyButton size="giant" variant="outline">Giant Outline</MyButton>
+      <MyButton size="large" variant="outline">Large Outline</MyButton>
+      <MyButton size="medium" variant="outline">Medium Outline</MyButton>
+      <MyButton size="small" variant="outline">Small Outline</MyButton>
+      <MyButton size="tiny" variant="outline">Tiny Outline</MyButton>
+    </div>
+<div>
+  
+      <MyButton size="giant" variant="ghost">Giant Ghost</MyButton>
+      <MyButton size="large" variant="ghost">Large Ghost</MyButton>
+      <MyButton size="medium" variant="ghost">Medium Ghost</MyButton>
+      <MyButton size="small" variant="ghost">Small Ghost</MyButton>
+      <MyButton size="tiny" variant="ghost">Tiny Ghost</MyButton>
+</div>
+
+    <div>
+      <MyButton icon>←</MyButton>
+      <MyButton variant="outline" icon>←</MyButton>
+      <MyButton variant="ghost" icon>←</MyButton>
+    </div>
+
+    <div>
+      <MyButton href="/about">Link Default</MyButton>
+      <MyButton href="/about" variant="outline">Link Outline</MyButton>
+      <MyButton href="/about" variant="ghost">Link Ghost</MyButton>
+      <MyButton href="/about" size="small">Link Small</MyButton>
+      <MyButton href="/about" size="large" variant="outline"> Link Large Outline </MyButton>
+    </div>
   </main>
 </template>
