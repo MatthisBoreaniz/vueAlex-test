335f3d7d8c6e33a88e0da2285069bb1ba3619188
Matthis Boreaniz <matthisonepiece@gmail.com>
2025-11-25 09:24:24 +0100

    add design system for buttons

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -9,46 +9,18 @@ defineProps<{
 <template>
   <a
     v-if="href"
-    class="button"
+    class="btn"
     :class="{ '-outline': variant === 'outline', '-small': size === 'small' }"
     href=""
     ><slot></slot
   ></a>
   <button
     v-else
-    class="button"
+    class="btn"
     :class="{ '-outline': variant === 'outline', '-small': size === 'small' }"
   >
     <slot></slot>
   </button>
 </template>
 
-<style lang="scss">
-.button {
-  --button-color: var(--color-text);
-  --button-bg-color: var(--color-primary);
-  --button-border-color: var(--color-primary);
-  padding: rem(10) rem(20);
-  background: var(--button-bg-color);
-  color: var(--button-color);
-  border: rem(1) solid var(--button-border-color);
-  border-radius: rem(4);
-  font-size: var(--font-size-base);
-  cursor: pointer;
-  &:hover {
-    --button-color: var(--color-primary);
-    --button-bg-color: transparent;
-  }
-  &.-small {
-    padding: rem(5) rem(10);
-  }
-  &.-outline {
-    --button-color: var(--color-primary);
-    --button-bg-color: transparent;
-    &:hover {
-      --button-color: var(--color-text);
-      --button-bg-color: var(--color-primary);
-    }
-  }
-}
-</style>
+
